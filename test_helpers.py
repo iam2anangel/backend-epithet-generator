b67c3b42d3d4408adfa09f8bfc59ce4381ec4dca
Jennifer Browning <browningjen@yahoo.com>
2019-04-20 19:57:49 -0400

    got one whole test to pass 453,678,123 hours later

--- test_helpers.py
+++ test_helpers.py
@@ -1,12 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# import pytest
+
+# from helpers import Vocabulary
+# from helpers import EpithetGenerator
+
+
+# json_data = Vocabulary.read_json("resources/data.json")
+# test_word = EpithetGenerator.generate_word()
+# test_words = EpithetGenerator.generate_words()
+
+# def test_read_json():
+#     assert isInstance(json_data, dict)
+#     assert len(json_data.keys()) == 3
+#     assert "artless" in json_data["Column 1"]
+#     assert "base-court" in json_data["Column 2"]
+#     assert "apple_john" in json_data["Column 3"]
+
+
+import pytest
+from helpers import Vocabulary
+from helpers import EpithetGenerator
+
+json_data = Vocabulary.read_json("resources/data.json")
+
+
+def test_read_json_happy():
+    assert isinstance(json_data, dict)
+    assert len(json_data.keys()) == 3
+    assert "bootless" in json_data["Column 1"]
+    assert "base-court" in json_data["Column 2"]
+    assert "coxcomb" in json_data["Column 3"]
+    
 
 
 
-def unit_test_vocab():
-    """Unit test (happy and sad) Vocabulary Class"""
-
-
-def unit_test_epithgen():
-    """Unit test the (happy and sad) EpithetGenerator class."""
