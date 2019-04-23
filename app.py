85c2db0d62a95df640e0fba60079a7d08a1efa4c
Jennifer Browning <browningjen@yahoo.com>
2019-04-23 12:54:42 -0400

    finished tests

--- app.py
+++ app.py
@@ -3,17 +3,17 @@
 
 from flask import Flask
 from flask import jsonify
+from backend_epithet_generator.helpers import EpithetGenerator, Vocabulary
 app = Flask(__name__)
 
 @app.route('/')
 def generate_epithets():
-    d = {"epithets": []}
+    result = EpithetGenerator().generate_word()
+    d = {"epithets": result}
     return jsonify(d)
-
-
-
 
 @app.route('/vocabulary')
 def vocabulary():
-    d = {"vocabulary": {}}
+    result = Vocabulary.read_json('resources/data.json')
+    d = {"vocabulary": result}
     return jsonify(d)
--- test_helpers.py
+++ test_helpers.py
@@ -5,35 +5,57 @@
 
 import pytest
 import os
-from backend_epithet_generator.helpers import Vocabulary
-from backend_epithet_generator.helpers import EpithetGenerator
+# from backend_epithet_generator import app
+from backend_epithet_generator.helpers import Vocabulary as V
+from backend_epithet_generator.helpers import EpithetGenerator as Epg
+# from backend_epithet_generator.helpers import EpithetGenerator
 
-json_data = Vocabulary.read_json("resources/data.json")
+
+json_data = V.read_json("resources/data.json")
+Ep = Epg().generate_word()
+
+
+
 
 
 def test_random_word():
-    assert isinstance(json_data, dict)
+    assert isinstance(single_ep, dict)
     assert len(json_data.keys()) == 3
     assert 'artless' in json_data['Column 1']
     assert 'base-court' in json_data['Column 2']
     assert 'apple-john' in json_data['Column 3']
-    assert 'Jen was here' not in json_data.keys()
 
 def test_random_word_fail():
     with pytest.raises(AssertionError):
         assert isinstance(json_data, str)
         assert len(json_data.keys()) == 'Not a Shakesperian word'
+        assert "Oopsie" not in json_data["Column 1"]
+        assert "Oopsie" not in json_data["Column 2"]
+        assert "Oopsie" not in json_data["Column 3"]
+
+
+def test_single_ep():
+    assert isinstance(Ep, str)
+    assert len(Ep.split(" ")) == 3
+    assert Ep.split(" ")[0] in json_data["Column 1"]
+    assert Ep.split(" ")[1] in json_data["Column 2"]
+    assert Ep.split(" ")[2] in json_data["Column 3"]
+
+
+def test_single_ep_fail():
+    with pytest.raises(AssertionError):
+        assert isinstance(Ep, dict)
+    with pytest.raises(AssertionError):
+        assert len(Ep.split(" ")) == "Flask is awesome"
+   
+
+   
+
+
+
     
 
-def test_epithet():
-    assert isinstance(EpithetGenerator.generate_words(), str)
-    assert len(EpithetGenerator.generate_words, list)
-    assert EpithetGenerator.generate_words()[1].split(' ') in json_data['Column 2']
 
-def test_epithet_fail():
-    with pytest(AssertionError):
-        assert isinstance(EpithetGenerator.generate_words(), str)
-        assert len(EpithetGenerator.generate_words()) == 50
    
 
    
